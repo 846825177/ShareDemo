f813c969a680ea46badcec710d5c7d67435e7f26
Mr Chen <846825177@qq.com>
2017-09-12 15:31:16 +0800

    曹小胖是三头猪

---
+++ app/src/main/java/com/chenzeyang/sharedemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.chenzeyang.sharedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //曹小胖是猪一头猪
+    }
+}
